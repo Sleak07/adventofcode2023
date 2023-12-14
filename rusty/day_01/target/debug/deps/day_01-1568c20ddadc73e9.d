042c8e3934e55976d7dd9cca2a1f7389a470e31b
Sleak07 <92532514+Sleak07@users.noreply.github.com>
2023-12-14 19:02:50 +0530

    Delete rusty/day_01 directory

--- rusty/day_01/target/debug/deps/day_01-1568c20ddadc73e9.d
+++
@@ -1,5 +0,0 @@
-/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-1568c20ddadc73e9: src/main.rs
-
-/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-1568c20ddadc73e9.d: src/main.rs
-
-src/main.rs:
--- rusty/day_01/target/debug/deps/day_01-437ef17cd881f6bc.d
+++
@@ -1,5 +0,0 @@
-/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/libday_01-437ef17cd881f6bc.rmeta: src/main.rs
-
-/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-437ef17cd881f6bc.d: src/main.rs
-
-src/main.rs:
