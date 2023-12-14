499bca6ff442284e23982ddd0c403f44643fa0b4
Bimal <trisky650@gmail.com>
2023-12-14 19:04:28 +0530

    Added the folder for rust

--- rusty/day_01/target/debug/deps/day_01-df8dea353cc0a0a3.d
+++
@@ -1,5 +0,0 @@
-/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/libday_01-df8dea353cc0a0a3.rmeta: src/main.rs
-
-/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-df8dea353cc0a0a3.d: src/main.rs
-
-src/main.rs:
