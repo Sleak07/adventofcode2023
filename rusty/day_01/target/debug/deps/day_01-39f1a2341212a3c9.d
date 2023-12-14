af17a847bb01560e40b119433051a9ae124fd7e4
Bimal <trisky650@gmail.com>
2023-12-14 22:27:33 +0530

    Read the contents of file properly

---
+++ rusty/day_01/target/debug/deps/day_01-39f1a2341212a3c9.d
@@ -0,0 +1,5 @@
+/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/libday_01-39f1a2341212a3c9.rmeta: src/main.rs
+
+/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-39f1a2341212a3c9.d: src/main.rs
+
+src/main.rs:
---
+++ rusty/day_01/target/debug/deps/day_01-9d37fd41dec5a5c1.d
@@ -0,0 +1,5 @@
+/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-9d37fd41dec5a5c1: src/main.rs
+
+/home/bimal/adventofcode2023/rusty/day_01/target/debug/deps/day_01-9d37fd41dec5a5c1.d: src/main.rs
+
+src/main.rs:
