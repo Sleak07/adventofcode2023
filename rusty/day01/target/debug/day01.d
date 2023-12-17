536450f99e54a17856843bf6d6a414f419c86c52
Bimal <trisky650@gmail.com>
2023-12-17 13:55:59 +0530

    stash

---
+++ rusty/day01/target/debug/day01.d
@@ -0,0 +1,1 @@
+/home/bimal/adventofcode2023/rusty/day01/target/debug/day01: /home/bimal/adventofcode2023/rusty/day01/src/main.rs
---
+++ rusty/day01/target/debug/deps/day01-cebb931f015c24a7.d
@@ -0,0 +1,5 @@
+/home/bimal/adventofcode2023/rusty/day01/target/debug/deps/day01-cebb931f015c24a7: src/main.rs
+
+/home/bimal/adventofcode2023/rusty/day01/target/debug/deps/day01-cebb931f015c24a7.d: src/main.rs
+
+src/main.rs:
